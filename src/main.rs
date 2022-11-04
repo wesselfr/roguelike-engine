6e38b676bac73a9c35136673aed317fdf68ad025
Wesselfr <wessel.frijters@gmail.com>
2022-11-04 11:29:01 +0100

    Removed example from main

--- src/main.rs
+++ src/main.rs
@@ -22,38 +22,10 @@ const BOX_SIZE: i16 = 64;
 
 /// Representation of the application state. In this example, a box will bounce around the screen.
 struct World {
-    player: Entity,
-    slimes: Vec<Entity>,
     now: Instant,
     time_passed: f32,
-    death_time: f32,
 }
 
-struct Entity {
-    pos: Vec2,
-    vel: Vec2,
-    sprite: DynamicImage,
-    alive: bool,
-}
-
-impl Entity {
-    fn new(pos: Vec2, vel: Vec2, sprite: DynamicImage) -> Self {
-        Self {
-            pos,
-            vel,
-            sprite,
-            alive: true,
-        }
-    }
-
-    fn update(&mut self, dt: f32) {
-        self.pos += self.vel * dt;
-    }
-
-    fn draw(&self, renderer: &mut Renderer) {
-        renderer.draw_sprite(self.pos, &self.sprite, 4);
-    }
-}
 
 fn main() -> Result<(), Error> {
     env_logger::init();
@@ -103,10 +75,6 @@ fn main() -> Result<(), Error> {
                 framework.resize(size.width, size.height);
             }
 
-            if input.key_pressed(VirtualKeyCode::R) {
-                world.reset();
-            }
-
             // Update internal state and request a redraw
             world.update(&mut input);
             window.request_redraw();
@@ -152,78 +120,12 @@ fn main() -> Result<(), Error> {
     });
 }
 
-// 1D Check
-fn collision_check(a: &Entity, b: &Entity) -> bool {
-    if a.pos.x + BOX_SIZE as f32 >= b.pos.x && a.pos.x <= b.pos.x + BOX_SIZE as f32
-    // && a.pos.y >= b.pos.y
-    // && a.pos.y <= b.pos.y + BOX_SIZE as f32
-    {
-        true
-    } else {
-        false
-    }
-}
-
 impl World {
     /// Create a new `World` instance that can draw a moving box.
     fn new() -> Self {
         Self {
-            // Player entity
-            player: Entity::new(
-                Vec2 {
-                    x: 24.0,
-                    y: HEIGHT as f32 / 2.0,
-                },
-                Vec2::ZERO,
-                image::open("src/assets/weapon_sword_1.png").unwrap(),
-            ),
-            // Slime entity
-            slimes: vec![
-                Entity::new(
-                    Vec2 {
-                        x: 300.0,
-                        y: HEIGHT as f32 / 2.0,
-                    },
-                    Vec2::ZERO,
-                    image::open("src/assets/slime_idle_anim_f0.png").unwrap(),
-                ),
-                Entity::new(
-                    Vec2 {
-                        x: 600.0,
-                        y: HEIGHT as f32 / 2.0,
-                    },
-                    Vec2::ZERO,
-                    image::open("src/assets/slime_idle_anim_f0.png").unwrap(),
-                ),
-                Entity::new(
-                    Vec2 {
-                        x: 900.0,
-                        y: HEIGHT as f32 / 2.0,
-                    },
-                    Vec2::ZERO,
-                    image::open("src/assets/slime_idle_anim_f0.png").unwrap(),
-                ),
-            ],
             now: Instant::now(),
             time_passed: 0.0,
-            death_time: 0.0,
-        }
-    }
-
-    fn reset(&mut self) {
-        self.death_time = 0.0;
-        self.player.alive = true;
-        self.player.pos = Vec2 {
-            x: 24.0,
-            y: HEIGHT as f32 / 2.0,
-        };
-
-        for (i, slime) in self.slimes.iter_mut().enumerate() {
-            slime.alive = true;
-            slime.pos = Vec2 {
-                x: 300.0 * (1 + i) as f32,
-                y: HEIGHT as f32 / 2.0,
-            }
         }
     }
 
@@ -232,84 +134,12 @@ impl World {
         let dt = self.now.elapsed().as_secs_f32();
         self.time_passed += dt;
         self.now = Instant::now();
-
-        // Friction
-        self.player.vel *= 0.8;
-        self.player.vel.y = 0.0;
-
-        // Player Input
-        if input.key_held(VirtualKeyCode::A) {
-            self.player.vel.x += -100.0;
-        }
-        if input.key_held(VirtualKeyCode::D) {
-            self.player.vel.x += 100.0;
-        }
-
-        // Bound check
-        if self.player.pos.x <= 0.0 || self.player.pos.x as u32 + BOX_SIZE as u32 > WIDTH {
-            self.player.vel.x *= -1.0;
-        }
-        if self.player.pos.y <= 0.0 || self.player.pos.y as u32 + BOX_SIZE as u32 > HEIGHT {
-            self.player.vel.y *= -1.0;
-        }
-
-        // Slime movement and collision
-        for slime in &mut self.slimes {
-            slime.vel.x = ((self.time_passed * 2.5).sin() * 80.0) - 40.0;
-            slime.vel.y = (self.time_passed * 1.5).cos() * 20.0;
-
-            if collision_check(&self.player, &slime) && slime.alive && self.player.alive {
-                println!("COLLISION");
-
-                if slime.vel.x < 0.0 {
-                    self.player.alive = false;
-                    self.death_time = self.time_passed;
-                } else {
-                    slime.alive = false;
-                }
-            }
-            slime.update(dt);
-        }
-
-        if !self.player.alive && self.time_passed - self.death_time > 3.0 {
-            self.reset();
-        }
-
-        self.player.update(dt);
     }
 
     /// Draw the `World` state to the frame buffer.
     ///
     /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
     fn draw(&self, renderer: &mut Renderer) {
-        let mut won = true;
-        for slime in &self.slimes {
-            if slime.alive {
-                won = false;
-            }
-        }
-        if !won {
-            renderer.clear_frame([0x48, 0xb2, 0xe8, 0xff]);
-        } else {
-            renderer.clear_frame([0x48, 0xe8, 0x5f, 0xff]);
-        }
-
-        if self.player.alive {
-            self.player.draw(renderer);
-        }
-
-        for slime in &self.slimes {
-            if slime.alive {
-                slime.draw(renderer);
-            }
-        }
-
-        let text = "Hello World!";
-        renderer.draw_text(Vec2 { x: 32.0, y: 30.0 }, text, 24.0, [0x00,0xff,0xff,0xff])
-
-        // Hide enemy
-        // if self.time_passed as u32 % 4 == 0{
-        //     self.slime.draw(renderer);
-        // }
+        
     }
 }
