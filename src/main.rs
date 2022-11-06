2298702f6c77bbdd4d01631f0933e7c3ef1e88b1
Wesselfr <wessel.frijters@gmail.com>
2022-11-06 14:47:22 +0100

    Removed gameplay logic

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 //use crate::easing::*;
 use crate::gui::Framework;
 use crate::renderer::*;
-use bitflags::bitflags;
 use glam::Vec2;
 use image::DynamicImage;
 use log::error;
 use pixels::Error;
-use std::cell::Cell;
 use std::time::Instant;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, VirtualKeyCode};
@@ -21,27 +19,9 @@ mod renderer;
 const WIDTH: u32 = 640;
 const HEIGHT: u32 = 480;
 
-const GRID_WIDTH: usize = 5;
-const GRID_HEIGHT: usize = 5;
-
-const GRID_OFFSET: Vec2 = Vec2 { x: 100.0, y: 100.0 };
-
-const TILE_COLOUR_A: [u8; 4] = [0xff, 0xff, 0xff, 0xff];
-const TILE_COLOUR_B: [u8; 4] = [0x00, 0x00, 0xff, 0xff];
-
-bitflags! {
-    struct CellType: u8
-    {
-        const PLAYER = 1 << 0;
-        const ENEMY = 1 << 1;
-    }
-}
-
 /// Representation of the application state. In this example, a box will bounce around the screen.
 struct World {
-    player_sprite: DynamicImage,
-    grid: [u8; GRID_WIDTH * GRID_HEIGHT],
-    new_grid: [u8; GRID_WIDTH * GRID_HEIGHT],
+    sprite: DynamicImage,
     now: Instant,
     time_passed: f32,
 }
@@ -121,7 +101,7 @@ fn main() -> Result<(), Error> {
                             context.scaling_renderer.render(encoder, render_target);
 
                             // Render egui
-                            framework.render(encoder, render_target, context);
+                            //framework.render(encoder, render_target, context);
 
                             Ok(())
                         });
@@ -143,80 +123,17 @@ impl World {
     /// Create a new `World` instance that can draw a moving box.
     fn new() -> Self {
         Self {
-            player_sprite: image::open("src/assets/weapon_sword_1.png").unwrap(),
-            grid: [0; 25],
-            new_grid: [0; 25],
+            sprite: image::open("src/assets/slime_idle_anim_f0.png").unwrap(),
             now: Instant::now(),
             time_passed: 0.0,
         }
     }
 
-    fn reset(&mut self) {
-        self.grid[2] = CellType::PLAYER.bits;
-        self.grid[8] = CellType::ENEMY.bits;
-    }
-
     /// Update the `World` internal state; bounce the box around the screen.
     fn update(&mut self, input: &mut WinitInputHelper) {
-        // Todo: Remove this way of initialization.
-        if self.time_passed == 0.0 {
-            self.reset()
-        };
-
         let dt = self.now.elapsed().as_secs_f32();
         self.time_passed += dt;
         self.now = Instant::now();
-
-        let mut player_dir_x: i32 = 0;
-        let mut player_dir_y: i32 = 0;
-
-        if input.key_pressed(VirtualKeyCode::W) {
-            player_dir_y = -1;
-        }
-        if input.key_pressed(VirtualKeyCode::S) {
-            player_dir_y = 1;
-        }
-        if input.key_pressed(VirtualKeyCode::A) {
-            player_dir_x = -1;
-        }
-        if input.key_pressed(VirtualKeyCode::D) {
-            player_dir_x = 1;
-        }
-
-        for y in 0..GRID_HEIGHT {
-            for x in 0..GRID_WIDTH {
-                let index = y * GRID_WIDTH + x;
-
-                if self.grid[index] == CellType::PLAYER.bits {
-                    let new_index = ((y as i32 + player_dir_y) * GRID_WIDTH as i32
-                        + x as i32
-                        + player_dir_x) as usize;
-
-                    if new_index != index {
-                        let pos_x = x as i32;
-                        let pos_y = y as i32;
-                        if pos_x + player_dir_x >= 0
-                            && pos_x + player_dir_x as i32 <= GRID_WIDTH as i32 - 1
-                            && pos_y + player_dir_y >= 0
-                            && pos_y + player_dir_y as i32 <= GRID_WIDTH as i32 - 1
-                        {
-                            self.new_grid[index] = 0;
-                            self.new_grid[new_index] = CellType::PLAYER.bits;
-                        }
-                    } else {
-                        self.new_grid[index] = CellType::PLAYER.bits;
-                    }
-                }
-                else if self.grid[index] == CellType::ENEMY.bits
-                {
-                    self.new_grid[index] = CellType::ENEMY.bits;
-                }
-            }
-        }
-
-        for i in 0..GRID_WIDTH * GRID_HEIGHT {
-            self.grid[i] = self.new_grid[i];
-        }
     }
 
     /// Draw the `World` state to the frame buffer.
@@ -231,57 +148,6 @@ impl World {
             [0xff, 0xff, 0xff, 0xff],
         );
 
-        for y in 0..GRID_HEIGHT {
-            for x in 0..GRID_WIDTH {
-                let index = y * GRID_WIDTH + x;
-
-                if index % 2 == 0 {
-                    renderer.draw_square(
-                        GRID_OFFSET
-                            + Vec2 {
-                                x: x as f32 * 32.0,
-                                y: y as f32 * 32.0,
-                            },
-                        Vec2::ONE * 32.0,
-                        TILE_COLOUR_A,
-                    );
-                } else {
-                    renderer.draw_square(
-                        GRID_OFFSET
-                            + Vec2 {
-                                x: x as f32 * 32.0,
-                                y: y as f32 * 32.0,
-                            },
-                        Vec2::ONE * 32.0,
-                        TILE_COLOUR_B,
-                    );
-                }
-
-                if self.grid[index] == CellType::PLAYER.bits {
-                    renderer.draw_sprite(
-                        GRID_OFFSET
-                            + Vec2 {
-                                x: x as f32 * 32.0,
-                                y: y as f32 * 32.0,
-                            },
-                        &self.player_sprite,
-                        2,
-                    );
-                }
-
-                if self.grid[index] == CellType::ENEMY.bits {
-                    renderer.draw_char(
-                        GRID_OFFSET
-                            + Vec2 {
-                                x: x as f32 * 32.0 + 8.0,
-                                y: y as f32 * 32.0 + 8.0,
-                            },
-                        'E',
-                        32.0,
-                        [0xea, 0x10, 0x26, 0xff],
-                    );
-                }
-            }
-        }
+        renderer.draw_sprite(Vec2 { x: 50.0, y: 50.0 }, &self.sprite, 4);
     }
 }
